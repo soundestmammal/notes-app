dac5ecffa01480f930ee5892bb50e5de6af36547
soundestmammal <robert.checco@gmail.com>
2018-01-18 02:01:24 -0500

    Node Modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,15 @@ console.log('Starting App.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+console.log(_.isString(true));
+console.log(_.isString('Robert'));
+
+
+
+
 
 // var user = os.userInfo();
 
